2c88d67275d4ce2f17e72e00c34709faf999835f
HieuVo1 <hieuvo044@gmail.com>
2021-02-08 21:52:51 +0700

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

b2bc2b8e8facade46b2e1c80008062fa695265f1
HieuVo1 <hieuvo044@gmail.com>
2021-03-03 20:27:12 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { throwError, of, iif, defer } from 'rxjs';
+import { catchError, map, retry, delay, defaultIfEmpty, throwIfEmpty, every } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  observer = {
+    next: value => console.log(value),
+    error: err => console.log(err),
+    complete: () => console.log("completed")
+  }
+
+  handlerError = () => {
+    console.log(`
+    -------------
+    I am handling the error. Alert to user
+    -------------
+    `)
+  }
+  ngOnInit(): void {
+    // throwError("I'm error").pipe(
+    //   catchError((err, caught) => {
+    //     this.handlerError();
+    //     return of("default value");
+    //   })
+    // )
+    //   .subscribe(this.observer);
+
+    //retry
+    // let cached = [4, 5];
+    // of(1, 2, 3, 4, 5)
+    //   .pipe(
+    //     map(n => {
+    //       if (cached.includes(n)) {
+    //         throw new Error("Duplicated: " + n);
+    //       }
+    //       return n;
+    //     }),
+    //     retry(3)
+    //   ).subscribe(this.observer);
+
+    // conditional
+    //defaultIfEmpty
+    // of().pipe(
+    //   delay(3000),
+    //   defaultIfEmpty("default value")
+    // ).subscribe(this.observer);
+
+    //throwIfEmpty
+    // of().pipe(
+    //   delay(3000),
+    //   throwIfEmpty(() => "empty value")
+    // ).subscribe(this.observer);
+
+    //every
+    // of(1, 2, 3, 4, 5)
+    //   .pipe(every(x => x < 6))
+    //   .subscribe(this.observer);
+
+    //iif chạy 2 function ngay cả khi chưa subscribe
+    let userId = 1;
+    function updateObservable() {
+      return of("update");
+    }
+
+    function createObservable() {
+      return of("create");
+    }
+    iif(() => userId == 2, updateObservable(), createObservable()).subscribe(this.observer);
+
+    defer(() => {
+      return userId == null ? updateObservable() : createObservable();
+    }).subscribe(this.observer);
+
+  }
 }
